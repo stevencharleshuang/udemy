f6722b1007c4a3efe4f7640c9c1fab9c7c639281
stevencharleshuang <steven.charles.huang@gmail.com>
2018-03-07 18:49:52 -0500

    008 Type Conversion

--- Modern JavaScript From The Beginning/Project/app.js
+++ Modern JavaScript From The Beginning/Project/app.js
@@ -65,27 +65,69 @@ Reference Data Types
 */
 // Primitive Types
 // String
-const name = 'John Doe';
-// Number
-const age = '45';
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+// const name = 'John Doe';
+// // Number
+// const age = '45';
+// // Boolean
+// const hasKids = true;
+// // Null
+// const car = null;
+// // Undefined
+// let test;
+// // Symbol
+// const sym = Symbol();
 
-// Refernce Types - Objects
-// Array
-const hobbies = ['movies', 'music'];
-// Object Literal
-const address = {
-    city: 'Boston',
-    state: 'MA'
-}
+// // Refernce Types - Objects
+// // Array
+// const hobbies = ['movies', 'music'];
+// // Object Literal
+// const address = {
+//     city: 'Boston',
+//     state: 'MA'
+// }
 const today = new Date();
 
 console.log(today);
 console.log(typeof today);
+
+/* Type Conversion */
+let val;
+
+// Num to Str
+val = String(555);
+val = String(4+4);
+// Bool to Str
+val = String(true);
+// Date to Str
+val = String(new Date());
+// Array to Str
+val = String([1,2,3,4]);
+
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to Num
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello'); //Nan 
+val = Number([1,2,3]); //Nan
+
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+// // Output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed());
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
+
